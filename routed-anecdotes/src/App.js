6013c2ed349a8c87b1621d918902059b95e57a5b
learntopilk <jonsterivilja@gmail.com>
2018-06-17 20:10:16 +0300

    Tehtävä 6.22 tehty

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Grid, Col, Row, } from 'react-bootstrap'
 
 const Menu = () => {
   const style = {
@@ -50,17 +50,35 @@ const DetailedAnecdote = ({ anec }) => (
 
 
 const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+  <Grid>
+    <Col xs={8}>
+      <div>
+        <h2>About anecdote app</h2>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+        <Row>
+          <p>According to Wikipedia:</p>
+        </Row>
+
+        <Row>
+          <Col xs={12} md={8}>
+            <em>An anecdote is a brief, revealing account of an individual person or an incident.
+              Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+              such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
+          </Col>
+          <Col xs={6} md={4}>
+            <img style={ { width: '220px' } } src="https://upload.wikimedia.org/wikipedia/commons/6/67/Babbage40.png" />
+          </Col>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
+        </Row>
+
+        <Row>
+          <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        </Row>
+
+      </div>
+    </Col>
+  </Grid>
 )
 
 const Footer = () => (
